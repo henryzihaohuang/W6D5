fb2c3e69046e20a072725220a5fb79ec98140873
Derek Chen <derekchen72@gmail.com>
2020-12-04 17:12:07 -0500

    cats

--- cathotel/app/models/cat.rb
+++ cathotel/app/models/cat.rb
@@ -1,8 +1,15 @@
+require 'action_view'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
 
-    validates :name
-    validates  :sex
-    validates :color, :inclusion => {:in => VALID_VALUES}
+    VALID_VALUES = ['Orange', 'Grey','Black', 'Calico']
 
+    validates :name, presence: true
+    validates  :sex, presence: true
+    validates :color, :inclusion => {:in => VALID_VALUES}, presence: true
 
+    def age
+        time_ago_in_words(birth_date)
+    end
 end
